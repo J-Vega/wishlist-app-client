1b857d323c22d0c5375d7d087b6457ba74eabf4b
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-03 12:49:06 -0400

    initial commit

---
+++ src/reducers/fetchreducer.js
@@ -0,0 +1,55 @@
+import {
+  FETCH_PRODUCTS_BEGIN,
+  FETCH_PRODUCTS_SUCCESS,
+  FETCH_PRODUCTS_FAILURE
+} from '../actions/walmartactions';
+
+const initialState = {
+  //state.products.items is here
+  items: [],
+  loading: false,
+  error: null
+};
+
+export default function fetchReducer(state = initialState, action) {
+  
+  switch(action.type) {
+    case FETCH_PRODUCTS_BEGIN:
+      // Mark the state as "loading" so we can show a spinner or something
+      // Also, reset any errors. We're starting fresh.
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+
+    case FETCH_PRODUCTS_SUCCESS:
+      // All done: set loading "false".
+      // Also, replace the items with the ones from the server
+      console.log("Fetch product successful.");
+      console.log(action.payload);
+
+      return {
+        ...state,
+        loading: false,
+        items: action.payload
+      }
+
+    case FETCH_PRODUCTS_FAILURE:
+      // The request failed, but it did stop, so set loading to "false".
+      // Save the error, and we can display it somewhere
+      // Since it failed, we don't have items to display anymore, so set it empty.
+      // This is up to you and your app though: maybe you want to keep the items
+      // around! Do whatever seems right.
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        items: []
+      };
+
+    default:
+      // ALWAYS have a default case in a reducer
+      return state;
+  }
+}
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import fetchReducer from "./fetchreducer";
+
+export default combineReducers({
+  fetchReducer
+});
