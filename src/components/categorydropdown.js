bf96930ed19ea08c5ffba2f17378b2e968d82a08
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-06 12:12:52 -0500

    added categories

--- src/components/categorydropdown.js
+++ src/components/categorydropdown.js
@@ -1,85 +1,40 @@
+
 import React from 'react'
 
 import "./categorydropdown.css";
 
-export default class CategoryDropdown extends React.Component{
-    
-  constructor() {
-    super();
-    
-    this.state = {
-      showMenu: false,
-    };
-    
-    this.showMenu = this.showMenu.bind(this);
-    this.closeMenu = this.closeMenu.bind(this);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-  
-  showMenu(event) {
-    event.preventDefault();
-    
-    this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu);
-    });
-  }
-  
-  closeMenu(event) {
-    
-    if (!this.dropdownMenu.contains(event.target)) {
-      
-      this.setState({ showMenu: false }, () => {
-        document.removeEventListener('click', this.closeMenu);
-      });  
-      
+const CategoryDropdown = (props) => {
+        console.log('re-render');
+        const data = props.data;
+        const clickHandler = props.clickHandler
+        const wishes = [
+                { wish:"gift", text:"Presents" },
+                { wish:"grocer", text:"Groceries" },
+                { wish:"events", text:"Events" },
+                { wish:"others", text:"Other "}
+            ];
+        return (
+            <div>
+                <div className="category-dropdown-container">
+                    <div className="buttons">
+                        {
+                            wishes.map(e => (
+                                <button
+                                    data-name={data.name} 
+                                    data-price={data.price}
+                                    data-image={data.mediumImage}
+                                    data-key={data.itemId}
+                                    data-url={data.productUrl}
+                                    data-wish= {e.wish}
+                                    data-text={e.text}
+                                    onClick={clickHandler}
+                                >{e.text}</button>
+                            ))
+                        } 
+                    </div>
+                </div>
+            </div>
+        );
     }
-  }
 
-  handleClick(category){
-    console.log("Adding item to category: " + category);
-    // fetch('/Wishlist', {
-    //   method: 'put',
-    //   body: JSON.stringify(item)
-    // })
-    // .then(function(response) {
-    //   console.log(response);
-    //   return response.json();
-    // })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.showMenu}>
-          Show menu
-        </button>
-        
-        {
-          this.state.showMenu
-            ? (
-              <div
-                className="menu"
-                ref={(element) => {
-                  this.dropdownMenu = element;
-                }}
-              >
-              <ul>
-                <li>
-                  <button className="category-button" ref={(element) => {this.handleClick("Gifts")}}>
-                    Gifts 
-                  </button>
-                </li>
-                <li><button className="category-button"> Essentials </button></li>
-                <li><button className="category-button"> Other </button></li>
-                </ul>
-              </div>
-            )
-            : (
-              null
-            )
-        }
-      </div>
-    );
-  }
-}
+export default CategoryDropdown;
