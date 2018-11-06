c1e2a01dd1ecbf1e0fbb0b7482c7993b4b27f038
Jillian Kim <jillianrkim12@gmail.com>
2018-11-05 18:43:17 -0800

    listing works now

--- src/components/searchform.js
+++ src/components/searchform.js
@@ -1,43 +1,27 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
+import thunk from 'redux-thunk';
+import $ from 'jquery';
 
+import SearchResults from './searchresults';
 import { fetchWalmartProducts } from "../actions/walmartactions";
 import { fetchEtsyProducts } from "../actions/etsyactions";
 import { fetchBestBuyProducts } from "../actions/bestbuyactions";
 import './searchform.css';
 
 const {API_BASE_URL} = require('../config');
+const walAPI = 'cwd2qzamfg6f523deuwhuxec';
+const YTAPI = 'AIzaSyAkcjnCcHDCNVjZXC28pvI8ur0GMxoAKTY';
 
 class SearchForm extends Component{
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
       inputValue: '',
-      resultObj:[]
+      result: []
     };
-    //this.handleSubmit = this.handleSubmit.bind(this);
+    this.search = this.search.bind(this);
   }
-
-  // handleSubmit(event){
-  //   event.preventDefault();
-  //   event.preventDefault();
-  //   console.log("Searching...")
-  //   //dispatch 3 separate actions
-  //   //dispatch action then have reducer => data from redux state using connect method utilize mapstateprop
-  //   let apiUrls = [
-  //     `${API_BASE_URL}/Walmart/Listings/?searchTerm=macbook`,
-  //     `${API_BASE_URL}/BestBuy/Listings/?searchTerm=macbook`,
-  //     `${API_BASE_URL}/Etsy/Listings/?searchTerm=macbook`
-  //     ]
-  //   Promise.all(apiUrls.map(url =>
-  //     fetch(url).then(response => response.json())
-  //   ))
-  //   //if data.canonical exists...do this if not it's walmart
-  //   .then(data => {
-  //     console.log(...data)
-  //   })
-  //   .catch(err => console.log(err));
-  //   };
 
   //Update text value to use when dispatching fetch requests
   updateInputValue(evt){
@@ -46,32 +30,96 @@ class SearchForm extends Component{
     })
   }
 
+  /*
+  Nov/4 comment
+    to get information: (Use Redux)
+    bind below fn to onClick
+    -- fix the async api call error !
+
+    -- And need more "Reduce" files for each dispatching to use redux module
+    -- take a look at a file: '/reducers/walreducer.js'
+    --------------------------
+    search(e){
+      e.preventDefault();
+      this.props.dispatch(fetchWalmartProducts(this.state.inputValue))
+      this.props.dispatch(fetchBestBuyProducts(this.state.inputValue));
+      this.props.dispatch(fetchEtsyProducts(this.state.inputValue));
+    }
+    ---------------------------
+  */
+
+  search(e){
+    e.preventDefault();
+
+    /*
+    Nov/4 comment
+      - why are you using the localhost url when getting api call from walmart?
+
+      -it will occur following error:
+      --SyntaxError: Unexpected token < in JSON at position 0
+
+      -the below api call will return plain HTML code of its index.html
+      -because the api call calling its localhost.
+    */
+
+    /*
+    Nov/5 comment
+
+    - Fetching does not work for walmart
+    - However, the logic below is correct
+
+    let apiUrls = [
+      `//api.walmartlabs.com/v1/search?query=${this.state.inputValue}&format=json&apiKey=${walAPI}`
+      // `${API_BASE_URL}/BestBuy/Listings/?searchTerm=macbook`,
+      // `${API_BASE_URL}/Etsy/Listings/?searchTerm=macbook`
+      ]
+    Promise.all(apiUrls.map(url =>
+      fetch(url)
+      .then(res => res.json())
+    ))
+    .then(data => {
+      this.setState({
+        result: data
+      })
+    })
+    .catch(err => console.log(err));
+    */
+
+    $.ajax({
+      url: `//api.walmartlabs.com/v1/search?query=${this.state.inputValue}&format=json&apiKey=${walAPI}`,
+      type: 'GET',
+      dataType: 'jsonp'
+    })
+    .done( res => {
+      console.log(res);
+      this.setState({
+        result: res.items
+      })
+    })
+    .fail( res => {
+      console.log("error", res);
+    });
+    
+  };
   render(){
-    	return (
-    		<div className="row">
+      return (
+        <div className="row">
             {/* <form className="js-search-form" onSubmit={this.handleSubmit}> */}
-            <form className="js-search-form" onSubmit={(e) => {
-                e.preventDefault();
-                console.log(this.state.inputValue);
-                this.props.dispatch(fetchWalmartProducts(this.state.inputValue))
-                this.props.dispatch(fetchBestBuyProducts(this.state.inputValue))
-                this.props.dispatch(fetchEtsyProducts(this.state.inputValue))}
-              }>
-              <div className ="col-1 search-container">
-                  </div>
-                  <div className ="col-2 search-container">
+            <form className="js-search-form" onSubmit={e => this.search(e)}>
+              <div className ="col-1 search-container"></div>
+              <div className ="col-2 search-container">
                   <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} type="text" className ="js-query searchBar searchTerm" required></input>
-                  </div>
-                  <div className ="col-2 search-container">
-              <button type ="submit" id= "searchButton" className ="searchButton">
+              </div>
+              <div className ="col-2 search-container">
+                  <button type ="submit" id= "searchButton" className ="searchButton">
                     Search Listings
                   </button>
-                  </div>
-            </form>   
+              </div>
+            </form>
+            <SearchResults result = {this.state.result}/>   
         </div>
-	);
-}
- 
+  );
+ }
 }
 
 const mapStateToProps = state => ({ 
--- src/components/searchresults.js
+++ src/components/searchresults.js
@@ -2,15 +2,7 @@ import React from 'react';
 import CategoryDropdown from './categorydropdown';
 import './searchresults.css';
 
-// import addToWishlist from '../actions/wishlistactions';
-
 export default function SearchResults({result}) {
-
-	// function handleClick(item, category){
-	// 	console.log("Adding item to wishlist");
-	// 	fetch(addToWishlist(item));
-	// }
-
 	return(
 		<div>
 			<div className="tab">
@@ -24,22 +16,21 @@ export default function SearchResults({result}) {
 			  <h3>BestBuy Results</h3>
 			  <div className='js-search-results-BESTBUY'>
 					{ 
-						!result.length ? '' : 
-						result[0].map(e => (
-							<div key={e.url}>
-								<div className="name">{e.name}</div>
-								<div className="price">$ {e.regularPrice}</div>
-								<img className="image" src={e.image} alt={e.name}/>
+						// !result.length ? '' : 
+						// result[0].map(e => (
+						// 	<div key={e.url}>
+						// 		<div className="name">{e.name}</div>
+						// 		<div className="price">$ {e.regularPrice}</div>
+						// 		<img className="image" src={e.image} alt={e.name}/>
 
-								{/* CALL ACTION ON CLICK, THIS SHOULD ONLY DISPLAY WHEN LOGGED IN */}
-								<button>ADD TO WISHLIST</button>
-								<CategoryDropdown/>
+						// 		{/* CALL ACTION ON CLICK, THIS SHOULD ONLY DISPLAY WHEN LOGGED IN */}
+						// 		<button>ADD TO WISHLIST</button>
+						// 		<CategoryDropdown/>
 
+						// 		<a href={e.url}>Buy Now!</a>
 
-								<a href={e.url}>Buy Now!</a>
-
-							</div>
-						))
+						// 	</div>
+						// ))
 					}
 				</div>
 			</div>
@@ -49,7 +40,7 @@ export default function SearchResults({result}) {
 			  <div className='js-search-results-WALMART'>
 				{ 
 						!result.length ? '' : 
-						result[1].map(i => (
+						result.map(i => (
 							<div key={i.url}>
 								<div className="name">{i.name}</div>
 								<div className="price">$ {i.salePrice}</div>
@@ -57,15 +48,8 @@ export default function SearchResults({result}) {
 
 
 								{/* CALL ACTION ON CLICK, THIS SHOULD ONLY DISPLAY WHEN LOGGED IN */}
-								{/* <button 
-
-								onClick={(this.props.handleClick(values =>
-										this.onClick(values))}>
-										
-										ADD TO WISHLIST</button> */}
-
-
-								{/* <CategoryDropdown/> */}
+								<button>ADD TO WISHLIST</button>
+								<CategoryDropdown/>
 
 
 								<a className="listing-url" href={i.productUrl}>Buy Now!</a>
@@ -80,20 +64,20 @@ export default function SearchResults({result}) {
 			  <h3>Etsy Results</h3>
 			  <div className='js-search-results-ETSY'>
 				{ 
-						!result.length ? '' : 
-						result[2].map(e => (
-							<div key={e.url}>
-								<div className="listing-name">{e.title}</div>
-								<div className="listing-price">$ {e.regularPrice}</div>
+						// !result.length ? '' : 
+						// result[2].map(e => (
+						// 	<div key={e.url}>
+						// 		<div className="listing-name">{e.title}</div>
+						// 		<div className="listing-price">$ {e.regularPrice}</div>
 
-								{/* CALL ACTION ON CLICK, THIS SHOULD ONLY DISPLAY WHEN LOGGED IN */}
-								<button>ADD TO WISHLIST</button>
-								<CategoryDropdown/>
+								
+						// 		<button>ADD TO WISHLIST</button>
+						// 		<CategoryDropdown/>
 
 
-								<a className="listing-url" href={e.url}>Buy Now!</a>
-							</div>
-						))
+						// 		<a className="listing-url" href={e.url}>Buy Now!</a>
+						// 	</div>
+						// ))
 					}
 				</div>
 			</div>
