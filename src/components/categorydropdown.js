48be3460f52824f7581c7466d6644b0de527679a
Jillian Kim <jillianrkim12@gmail.com>
2018-11-05 22:26:16 -0800

    adding to wishlist

--- src/components/categorydropdown.js
+++ src/components/categorydropdown.js
@@ -2,29 +2,38 @@ import React from 'react'
 
 import "./categorydropdown.css";
 
-export default class CategoryDropdown extends React.Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        categories: []
-      }
-      
-     
+const CategoryDropdown = (props) => {
+        console.log('re-render');
+        const data = props.data;
+        const clickHandler = props.clickHandler
+        const wishes = [
+                { wish:"gift", text:"Presents" },
+                { wish:"grocer", text:"Groceries" },
+                { wish:"events", text:"Events" },
+                { wish:"others", text:"Other "}
+            ];
+        return (
+            <div>
+                <div className="category-dropdown-container">
+                    <div className="buttons">
+                        {
+                            wishes.map(e => (
+                                <button
+                                    data-name={data.name} 
+                                    data-price={data.price}
+                                    data-image={data.mediumImage}
+                                    data-key={data.itemId}
+                                    data-url={data.productUrl}
+                                    data-wish= {e.wish}
+                                    data-text={e.text}
+                                    onClick={clickHandler}
+                                >{e.text}</button>
+                            ))
+                        } 
+                    </div>
+                </div>
+            </div>
+        );
     }
 
-    render(){
-        return true;
-        // return (
-        //     <div className="category-dropdown-container">
-        //         <button>Add to wishlist</button> 
-        //             {/* //Drop down of caegories */}
-
-            
-        //     </div>
-        //     <button>Presents</button> 
-        //     <button>Groceries</button> 
-        //     <button>Events</button> 
-        //     <button>Other</button> 
-        // );
-    }
-}
+export default CategoryDropdown;
