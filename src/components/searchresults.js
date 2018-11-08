d164b87ee6594248c04779247327bf9f1a83e430
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-08 15:18:50 -0500

    Added fetch requests to add product to wishlist by username and category

--- src/components/searchresults.js
+++ src/components/searchresults.js
@@ -2,6 +2,12 @@ import React from 'react';
 import CategoryDropdown from './categorydropdown';
 import './searchresults.css';
 import WishLists from './wishlist';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import WalmartResults from './walmartresults';
+
+import {API_BASE_URL} from '../config';
+
 
 class SearchResults extends React.Component{
 	constructor(props){
@@ -15,18 +21,22 @@ class SearchResults extends React.Component{
         others: []
       }
       this.wishRouter = this.wishRouter.bind(this);
-    }
+	}
+	
+	
 
 	wishRouter(e){
         e.preventDefault();
         let wishPack = {
             name: e.target.dataset.name,
-            price: e.target.dataset.price,
+			price: e.target.dataset.price,
+			msrp:e.target.dataset.msrp,
             image: e.target.dataset.image,
             url: e.target.dataset.url
         }
 
         if(e.target.dataset.wish === "gift")   {
+			this.addToList(wishPack, e.target.dataset.wish);
             this.setState(
                 {gift: this.state.gift.concat(wishPack)},
                 () => {console.log('Gift list now: ', this.state.gift)}
@@ -34,6 +44,7 @@ class SearchResults extends React.Component{
         }
 
         if(e.target.dataset.wish === "grocer") {
+			this.addToList(wishPack, e.target.dataset.wish);
             this.setState(
                 {grocer: this.state.grocer.concat(wishPack)},
                 () => {console.log('Grocery list now: ', this.state.grocer)}
@@ -42,14 +53,19 @@ class SearchResults extends React.Component{
         }
 
         if(e.target.dataset.wish === "events") {
+			//e.target.dataset.wish = Category for fetch request
+			this.addToList(wishPack, e.target.dataset.wish);
             this.setState(
-                {events: this.state.events.concat(wishPack)},
+				{
+					events: this.state.events.concat(wishPack)
+				},
                 () => {console.log('Events list now: ', this.state.events)}
             );
                         
         }
 
         if(e.target.dataset.wish === "others") {
+			this.addToList(wishPack, e.target.dataset.wish);
             this.setState(
                 {others: this.state.others.concat(wishPack)},
                 () => {console.log('Others list now: ', this.state.others)}
@@ -57,83 +73,121 @@ class SearchResults extends React.Component{
                         
         }
 
-    }
+	}
+	
+	addToList(itemData, category){
+		console.log(itemData);
+		//5bc91e143d799d3ae4f0ce41    ******Need to obtain wishlist ID   
+		//	OR   update db by locating wishlist by username and category
+		let itemPrice = `$${itemData.price}`;
+		if(itemPrice === undefined) {
+			console.log("Sale price is null, using msrp instead");
+			itemPrice=`$${itemData.msrp}`;
+		};
+		if(itemPrice === undefined){
+			console.log("MSRP is undefined as well! Set price to N/A");
+			itemPrice="N/A";
+		}
+		if(itemData.price)
+		fetch(`${API_BASE_URL}/WishList/User/${window.localStorage.userName}/${category}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+				"id":"5bc91e143d799d3ae4f0ce41",
+				"category":category,
+				"name":`${itemData.name}`,
+				"price":`${itemData.price}`,
+				"imageUrl":`${`${itemData.image}`}`,
+				"link":`${itemData.url}`
+            }) 
+        })
+	}
 
 	render(){
+
+
+
 		return(
-			<div>
-				<div className="tab">
-				  <button className="tablinks" id="defaultOpen">Walmart</button>
-				  <button className="tablinks">Best Buy</button>
-				  <button className="tablinks">Etsy</button>
-				</div>
+			<div className="results-container">
+			<Tabs className="tab">
+				<TabList>
+				  <Tab className="tablinks" id="defaultOpen">Walmart</Tab>
+				  <Tab className="tablinks">Best Buy</Tab>
+				  <Tab className="tablinks">Etsy</Tab>
+				</TabList>
+			
 
 				
-				<div id="BestBuy-Tab" className="tabcontent">
-				  <h3>BestBuy Results</h3>
-				  <div className='js-search-results-BESTBUY'>
-						{ 
-							// !result.length ? '' : 
-							// result[0].map(e => (
-							// 	<div key={e.url}>
-							// 		<div className="name">{e.name}</div>
-							// 		<div className="price">$ {e.regularPrice}</div>
-							// 		<img className="image" src={e.image} alt={e.name}/>
-
-							// 		{/* CALL ACTION ON CLICK, THIS SHOULD ONLY DISPLAY WHEN LOGGED IN */}
-							// 		<button>ADD TO WISHLIST</button>
-							// 		<CategoryDropdown/>
-
-							// 		<a href={e.url}>Buy Now!</a>
-
-							// 	</div>
-							// ))
-						}
-					</div>
-				</div>
-
-				<div id="Walmart-Tab" className="tabcontent">
-				  <h3>Walmart Results</h3>
+				
+				<TabPanel id="Walmart-Tab" className="tabcontent">
+				  <h3 className="results-header">Walmart Results</h3>
 				  <div className='js-search-results-WALMART'>
 					{ 
-							!this.props.result.length ? '' : 
-							this.props.result.map(i => (
-								<div key={i.itemId}>
-									<div className="name">{i.name}</div>
-									<div className="price">$ {i.salePrice}</div>
-									<img className="image" src={i.mediumImage} alt={i.name}/>
-									
-									<CategoryDropdown data={i} clickHandler={this.wishRouter}/>
-									<a className="listing-url" href={i.productUrl}>Buy Now!</a>
+						// !this.props.results['walmartResults'].length ? '' : 
+						// this.props.results['walmartResults'].map(i => (
+						!this.props.results["walmartResults"].length ? '' : 
+						this.props.results["walmartResults"].map(i => (
+							<div key={i.itemId}>
+								<div className="name">{i.name}</div>
+								<div className="price">$ {i.salePrice}</div>
+								<img className="image" src={i.mediumImage} alt={i.name}/>
+								
+								<CategoryDropdown data={i} clickHandler={this.wishRouter}/>
+								<a className="listing-url" href={i.productUrl}>Buy Now!</a>
+							</div>
+						))
+					}
+					
+					</div>
+				</TabPanel>
+
+				<TabPanel id="BestBuy-Tab" className="tabcontent">
+				  <h3 className="results-header">Best Buy Results</h3>
+				  <div className='js-search-results-BESTBUY'>
+						{ 
+							!this.props.results["bestbuyResults"].length ? '' : 
+							this.props.results["bestbuyResults"].map(e => (
+								<div key={e.url}>
+									<a className="name" href={e.url}>{e.name}</a>
+									<div className="price">$ {e.regularPrice}</div>
+									<img className="image" src={e.image} alt={e.name}/>
+
+									{/* CALL ACTION ON CLICK, THIS SHOULD ONLY DISPLAY WHEN LOGGED IN */}
+									<button>ADD TO WISHLIST</button>
+									<CategoryDropdown data={e} clickHandler={this.wishRouter}/>
+
+									<a href={e.url}>Buy Now!</a>
+
 								</div>
 							))
 						}
-					
 					</div>
-				</div>
+				</TabPanel>
 
-				<div id="Etsy-Tab" className="tabcontent">
-				  <h3>Etsy Results</h3>
+
+				<TabPanel id="Etsy-Tab" className="tabcontent">
+				  <h3 className="results-header">Etsy Results</h3>
 				  <div className='js-search-results-ETSY'>
 					{ 
-							// !result.length ? '' : 
-							// result[2].map(e => (
-							// 	<div key={e.url}>
-							// 		<div className="listing-name">{e.title}</div>
-							// 		<div className="listing-price">$ {e.regularPrice}</div>
-
-									
-							// 		<button>ADD TO WISHLIST</button>
-							// 		<CategoryDropdown/>
+							!this.props.results["etsyResults"].length ? '' : 
+							this.props.results["etsyResults"].map(e => (
+								<div key={e.url}>
+									<div className="listing-name">{e.title}</div>
+									<div className="listing-price">$ {e.regularPrice}</div>
+									<button>ADD TO WISHLIST</button>
+									<CategoryDropdown data={e} clickHandler={this.wishRouter}/>
 
 
-							// 		<a className="listing-url" href={e.url}>Buy Now!</a>
-							// 	</div>
-							// ))
+									<a className="listing-url" href={e.url}>Buy Now!</a>
+								</div>
+							))
 						}
 					</div>
-				</div>
+				</TabPanel>
 				{/*<WishLists wishes = {this.state.saveWishes} />*/}
+				</Tabs>
 			</div>
 			)
 	}
