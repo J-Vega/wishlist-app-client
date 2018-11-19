a8daa90eeccb11e0dcc28ffface51b60eb52c2b5
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-18 20:29:30 -0500

    removed comments

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import TopNav from './top-nav';
 import SearchForm from './searchform';
 import InfoSection from './info-section';
 import RegistrationPage from './registration-page';
-import WishList from './wishlist';
+
 import Footer from './footer';
 import {refreshAuthToken} from '../actions/auth';
 
@@ -51,7 +51,6 @@ export class App extends React.Component {
           <Route exact path="/" component={InfoSection} />
           <Route exact path="/" component={SearchForm}/>
           <Route exact path="/register" component={RegistrationPage} />
-          {/* <Route exact path="/wishlist" component={WishList} /> */}
           <Footer/>
         </header>
       </div>
