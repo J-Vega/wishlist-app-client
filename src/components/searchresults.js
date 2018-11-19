fd582a97366ca037e9b9cd2311bfe92407bc1010
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-18 20:33:05 -0500

    changed position of buy now button in best buy and etsy, added generic etsy logo url to images

--- src/components/searchresults.js
+++ src/components/searchresults.js
@@ -4,214 +4,198 @@ import './searchresults.css';
 import WishLists from './wishlist';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
+import { API_BASE_URL } from '../config';
 
 
-import {API_BASE_URL} from '../config';
-
-
-class SearchResults extends React.Component{
-	constructor(props){
-      super(props);
-      this.state = {
-        isButton: false,
-        isWish: false,
-        gift: [],
-        grocer: [],
-        events: [],
-		others: []
-      }
-      this.wishRouter = this.wishRouter.bind(this);
+class SearchResults extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isButton: false,
+			isWish: false,
+			gift: [],
+			grocer: [],
+			events: [],
+			others: []
+		}
+		this.wishRouter = this.wishRouter.bind(this);
 	}
-	
-	
 
-	wishRouter(e){
-        e.preventDefault();
-        let wishPack = {
-            name: e.target.dataset.name,
+	wishRouter(e) {
+		e.preventDefault();
+		let wishPack = {
+			name: e.target.dataset.name,
 			price: e.target.dataset.price,
-			msrp:e.target.dataset.msrp,
-            image: e.target.dataset.image,
-            url: e.target.dataset.url
-        }
+			msrp: e.target.dataset.msrp,
+			image: e.target.dataset.image,
+			url: e.target.dataset.url
+		}
 
-        if(e.target.dataset.wish === "gift")   {
+		if (e.target.dataset.wish === "gift") {
 			this.addToList(wishPack, e.target.dataset.wish);
-            this.setState(
-                {gift: this.state.gift.concat(wishPack)},
-                () => {console.log('Gift list now: ', this.state.gift)}
-            );
-        }
+			this.setState(
+				{ gift: this.state.gift.concat(wishPack) },
+				() => { console.log('Gift list now: ', this.state.gift) }
+			);
+		}
 
-        if(e.target.dataset.wish === "grocer") {
+		if (e.target.dataset.wish === "grocer") {
 			this.addToList(wishPack, e.target.dataset.wish);
-            this.setState(
-                {grocer: this.state.grocer.concat(wishPack)},
-                () => {console.log('Grocery list now: ', this.state.grocer)}
-            );
-                        
-        }
+			this.setState(
+				{ grocer: this.state.grocer.concat(wishPack) },
+				() => { console.log('Grocery list now: ', this.state.grocer) }
+			);
 
-        if(e.target.dataset.wish === "events") {
+		}
+
+		if (e.target.dataset.wish === "events") {
 			//e.target.dataset.wish = Category for fetch request
 			this.addToList(wishPack, e.target.dataset.wish);
-            this.setState(
+			this.setState(
 				{
 					events: this.state.events.concat(wishPack)
 				},
-                () => {console.log('Events list now: ', this.state.events)}
-            );
-                        
-        }
+				() => { console.log('Events list now: ', this.state.events) }
+			);
 
-        if(e.target.dataset.wish === "others") {
+		}
+
+		if (e.target.dataset.wish === "others") {
 			this.addToList(wishPack, e.target.dataset.wish);
-            this.setState(
-                {others: this.state.others.concat(wishPack)},
-                () => {console.log('Others list now: ', this.state.others)}
-            );
-                        
-        }
+			this.setState(
+				{ others: this.state.others.concat(wishPack) },
+				() => { console.log('Others list now: ', this.state.others) }
+			);
+
+		}
 
 	}
-	
-	addToList(itemData, category){
+
+	addToList(itemData, category) {
 		console.log(itemData);
 		//5bc91e143d799d3ae4f0ce41    ******Need to obtain wishlist ID   
 		//	OR   update db by locating wishlist by username and category
 		let itemPrice = `$${itemData.price}`;
-		if(itemPrice === undefined) {
+		if (itemPrice === undefined) {
 			console.log("Sale price is null, using msrp instead");
-			itemPrice=`$${itemData.msrp}`;
+			itemPrice = `$${itemData.msrp}`;
 		};
-		if(itemPrice === undefined){
+		if (itemPrice === undefined) {
 			console.log("MSRP is undefined as well! Set price to N/A");
-			itemPrice="N/A";
+			itemPrice = "N/A";
 		}
-		
+
 		fetch(`${API_BASE_URL}/Wishlist/User/${window.localStorage.userName}/${category}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-				
-				"category":category,
-				"name":`${itemData.name}`,
-				"price":`${itemData.price}`,
-				"imageUrl":`${`${itemData.image}`}`,
-				"link":`${itemData.url}`
-            }) 
-        })
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+
+				"category": category,
+				"name": `${itemData.name}`,
+				"price": `${itemData.price}`,
+				"imageUrl": `${`${itemData.image}`}`,
+				"link": `${itemData.url}`
+			})
+		})
 	}
 
-	findImage(itemId){
+	findImage(itemId) {
 		fetch(`${API_BASE_URL}/Etsy/Listing/Images/?listingId=${itemId}`)
 			.then(results => {
 				return results.json();
 			})
 			.then(data => {
 				//update state
-				this.setState({img:data.results[0].url_170x135});
-				console.log(data.results[0].url_170x135); 
+				this.setState({ img: data.results[0].url_170x135 });
+				console.log(data.results[0].url_170x135);
 			})
 	}
 
-	render(){
-		return(
-			
+	render() {
+		return (
+
 			<Tabs className="tab">
 				<TabList>
-				
-				  <Tab className="tablinks" id="defaultOpen">Walmart</Tab>
-				  <Tab className="tablinks">Best Buy</Tab>
-				  <Tab className="tablinks">Etsy</Tab>
-				  <Tab className="tablinks">My Wishlist</Tab>
+					<Tab className="tablinks" id="defaultOpen">Walmart</Tab>
+					<Tab className="tablinks">Best Buy</Tab>
+					<Tab className="tablinks">Etsy</Tab>
+					<Tab className="tablinks">My Wishlist</Tab>
 				</TabList>
-	
-				<TabPanel id="Walmart-Tab" className="tabcontent">
-				  <h3 className="results-header">Walmart Results</h3>
-				  <div className='js-search-results-WALMART'>
-					{ 
-						// !this.props.results['walmartResults'].length ? '' : 
-						// this.props.results['walmartResults'].map(i => (
-						!this.props.results["walmartResults"].length ? '' : 
-						this.props.results["walmartResults"].map(i => (
-							
-							<div className="item-container" key={i.itemId}>
-								<div className="image-container">
-									<img className="image" src={i.mediumImage} alt={i.name}/>
-								</div>
-								<div className="product-info-container">
-									<p className="name">{i.name}</p>
-									<div className="price">$ {i.salePrice}</div>
-									<button className="listing-url" href={i.productUrl}>Buy Now!</button>
-									
-									<CategoryDropdown data={i} clickHandler={this.wishRouter}/>
 
-									
-								</div>				
-							</div>
-						))
-					}
-					
+				<TabPanel id="Walmart-Tab" className="tabcontent">
+					<h3 className="results-header">Walmart Results</h3>
+					<div className='js-search-results-WALMART'>
+						{
+							!this.props.results["walmartResults"].length ? '' :
+								this.props.results["walmartResults"].map(i => (
+
+									<div className="item-container" key={i.itemId}>
+										<div className="image-container">
+											<img className="image" src={i.mediumImage} alt={i.name} />
+										</div>
+										<div className="product-info-container">
+											<p className="name">{i.name}</p>
+											<div className="price">$ {i.salePrice}</div>
+											<button className="listing-url" href={i.productUrl}>Buy Now!</button>
+											<CategoryDropdown data={i} clickHandler={this.wishRouter} />
+										</div>
+									</div>
+								))
+						}
+
 					</div>
 				</TabPanel><TabPanel id="BestBuy-Tab" className="tabcontent">
-				  <h3 className="results-header">Best Buy Results</h3>
-				  <div className='js-search-results-BESTBUY'>
-						{ 
-							!this.props.results["bestbuyResults"].length ? '' : 
-							this.props.results["bestbuyResults"].map(e => (
-								<div className="item-container" key={e.url}>
-									
-									<div className="image-container">
-										<img className="image" src={e.image} alt={e.name}/>
+					<h3 className="results-header">Best Buy Results</h3>
+					<div className='js-search-results-BESTBUY'>
+						{
+							!this.props.results["bestbuyResults"].length ? '' :
+								this.props.results["bestbuyResults"].map(e => (
+									<div className="item-container" key={e.url}>
+										<div className="image-container">
+											<img className="image" src={e.image} alt={e.name} />
+										</div>
+										<div className="product-info-container">
+											<a className="name" href={e.url}>{e.name}</a>
+											<div className="price">$ {e.msrp}</div>
+											<button className="listing-url" href={e.url}>Buy Now!</button>
+											<CategoryDropdown data={e} clickHandler={this.wishRouter} />
+										</div>
 									</div>
-									<div className="product-info-container">
-									<a className="name" href={e.url}>{e.name}</a>
-									<div className="price">$ {e.regularPrice}</div>
-
-									<CategoryDropdown data={e} clickHandler={this.wishRouter}/>
-									<button className="listing-url" href={e.url}>Buy Now!</button>
-									</div>						
-								</div>
-
-								
-							))
+								))
 						}
 					</div>
 				</TabPanel><TabPanel id="Etsy-Tab" className="tabcontent">
-				  <h3 className="results-header">Etsy Results</h3>
-				  <div className='js-search-results-ETSY'>
-					{ 
-							!this.props.results["etsyResults"].length ? '' : 
-							this.props.results["etsyResults"].map(e => (
-								<div className="item-container" key={e.name + e.url}>
-								<div className="image-container">
-									{/* <div>{this.findImage(e.listing_id)}</div> */}
-										<img className="image" src={this.state.img} alt={e.name}/>
+					<h3 className="results-header">Etsy Results</h3>
+					<div className='js-search-results-ETSY'>
+						{
+							!this.props.results["etsyResults"].length ? '' :
+								this.props.results["etsyResults"].map(e => (
+									<div className="item-container" key={e.name + e.url}>
+										<div className="image-container">
+											<img className="image" src={"https://upload.wikimedia.org/wikipedia/commons/a/aa/Etsy_logo_lg_rgb.png"} alt={e.name} />
+										</div>
+										<div className="product-info-container">
+											<div className="name">{e.title}</div>
+											<div className="price">$ {e.price}</div>
+											<button className="listing-url" href={e.url}>Buy Now!</button>
+											<CategoryDropdown data={e} clickHandler={this.wishRouter} />
+										</div>
 									</div>
-								<div className="product-info-container">
-									<div className="name">{e.title}</div>
-									<div className="price">$ {e.regularPrice}</div>
-									
-									<CategoryDropdown data={e} clickHandler={this.wishRouter}/>
-									<a className="listing-url" href={e.url}>Buy Now!</a>
-								</div>
-								</div>
-							))
+								))
 						}
 					</div>
 				</TabPanel>
 				<TabPanel>
-					<WishLists wishes = {this.state.gift} />
+					<WishLists wishes={this.state.gift} />
 				</TabPanel>
-				
-				</Tabs>
-	
-			)
+
+			</Tabs>
+
+		)
 	}
 }
-			
+
 
 export default SearchResults;
