1deb08699adc84dc53e8d3fe00cfed63dc8ceede
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-18 20:28:46 -0500

    revised info list

--- src/components/info-section.js
+++ src/components/info-section.js
@@ -9,14 +9,15 @@ export default function InfoSection(props) {
             UNICART 
         </p>
         <p className="info-sub-header">The only stop you need to browse and shop!</p>
-        <img src={'logo.jpg'} className="main-image"/>
+        <img src={'logo.jpg'} alt="A woman shopping within an aisle containing products from Walmart, Amazon, and Etsy" className="main-image"/>
 
         <p className="info-header">
             
         </p>
-        <p className="description">A more convenient way to shop online!</p>
-        <p className="description">Find product and keep them all in one place.</p>
-        <p className="description">Use one search bar to look up Walmart, Best Buy, and Etsy (with more to come)!</p>
+        <p className="description">A more convenient way to window shop online!</p>
+        <p className="description">Use one search bar to find product listings from Walmart, Best Buy, and Etsy (with more to come)!</p>
+        <p className="description">Select a category to add the product to your wishlist to save it for later!</p>
+        <p className="description">Ready to buy something on your list? Click on its 'Buy Now' button to go straight to its source!</p>
         {/* <p className="description">Use one search bar to browse through Walmart, Best Buy, and Etsy (more to come)! Create an account to manage a wishlist with multiple categories without having to navigate to every website!</p> */}
     </div>
   );
