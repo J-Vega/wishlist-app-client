9ae2c4c6449065691720c2cb634faf6152555d10
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-18 20:35:14 -0500

    Removed comments - data reaches recuder successfully

--- src/actions/wishlistactions.js
+++ src/actions/wishlistactions.js
@@ -1,22 +1,41 @@
 import {API_BASE_URL} from "../config";
 
-export const addToWishlist = (product) => dispatch => {
-    fetch(`${API_BASE_URL}/users/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'        
-      },
-      body: JSON.stringify(product)
-    })
-    .then(res => res.json())
-    .then(response => {
-     console.log("Added Item to wishlist!");
-    })
-    .catch(error => console.log(error))
-  }
+export const wishListActions = username => dispatch => {
 
-  const ADD_SUCCESS = 'ADD_SUCCESS'
-  export const addToWishlistSuccess = product => ({
-    type: addToWishlistSuccess,
-    product
-  })
+  fetch(`${API_BASE_URL}/Wishlist/User/${username}`)
+  .then(res => res.json())
+  .then(data => {
+  (data.map(i => {
+    //console.log(i.wishlists.category);
+    //console.log(...(i.wishlists.items));
+    dispatch(findWishListSuccess(i.wishlists));
+  }));
+})
+  .catch(error => console.log(error))
+}
+
+
+  // Handle HTTP errors since fetch won't.
+// function handleErrors(response) {
+//   if (!response.ok) {
+//     throw Error(response.statusText);
+//   }
+//   return response;
+// }
+  export const FIND_WISHLIST_BEGIN = 'FIND_WISHLIST_BEGIN'
+  export const findWishListBegin = wishlist => ({
+    type: FIND_WISHLIST_BEGIN,
+    wishlist
+  });
+
+  export const FIND_WISHLIST_SUCCESS = 'FIND_WISHLIST_SUCCESS'
+  export const findWishListSuccess = wishlist => ({
+    type: FIND_WISHLIST_SUCCESS,
+    wishlist
+  });
+
+  export const FIND_WISHLIST_FAILURE = 'FIND_WISHLIST_FAILURE'
+  export const findWishListFailure = wishlist => ({
+    type: FIND_WISHLIST_FAILURE,
+    wishlist
+  });
